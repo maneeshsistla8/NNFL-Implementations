c10d9e03f108f33f5cf8fd7a58f119a078da82fb
Maneesh Sistla <maneesh.m2wifcs@gmail.com>
2019-10-15 13:29:18 +0530

    Fixed One vs One Classification

--- Assignment1/q8.2.py
+++ Assignment1/q8.2.py
@@ -7,15 +7,15 @@ from mpl_toolkits.mplot3d import Axes3D
 def sigmoid(z):
 	return 1/(1 + np.exp(-z))
 
-def LogisticRegression(train, theta, iters, alpha, class_label):
+def LogisticRegression(train, theta, iters, alpha, a, b):
 	X = np.array(train.iloc[:, :-1])
 	Y = np.array(train.iloc[:, -1])
 	m = Y.shape[0]
 
 	for i in range(len(Y)):
-		if(Y[i] != class_label):
+		if(Y[i] == b):
 			Y[i] = 0
-		else:
+		elif(Y[i] == a):
 			Y[i] = 1
 			
 	for i in range(iters):
@@ -27,15 +27,15 @@ def LogisticRegression(train, theta, iters, alpha, class_label):
 
 	return theta
 
-def Accuracy(test, theta_1, theta_2, theta_3):
+def Accuracy(test, theta_12, theta_13, theta_23):
 	accuracy, accuracy_1, accuracy_2, accuracy_3 = 0, 0, 0, 0
 	m1, m2, m3 = 0, 0, 0
 
 	X = np.array(test.iloc[:, :-1])
 	Y = np.array(test.iloc[:, -1])
-	z1 = np.dot(X, theta_1)
-	z2 = np.dot(X, theta_2)
-	z3 = np.dot(X, theta_3)
+	z1 = np.dot(X, theta_12)
+	z2 = np.dot(X, theta_13)
+	z3 = np.dot(X, theta_23)
 	h1 = sigmoid(z1)
 	h2 = sigmoid(z2)
 	h3 = sigmoid(z3)
@@ -55,11 +55,11 @@ def Accuracy(test, theta_1, theta_2, theta_3):
 		b = 1 if h2[index] > 0.5 else 0
 		c = 1 if h3[index] > 0.5 else 0
 		c1 += (a == 1)
-		c1 += (c == 0)
+		c1 += (b == 1)
 		c2 += (a == 0)
-		c2 += (b == 1)
+		c2 += (c == 1)
 		c3 += (b == 0)
-		c3 += (c == 1)
+		c3 += (c == 0)
 		max_class = max(c1, c2, c3)
 		if(c1 == max_class):
 			a = 1
@@ -94,16 +94,16 @@ data.iloc[:, 1:-1] = (to_norm - to_norm.mean())/to_norm.std()
 train = data.sample(frac=0.6, random_state=random.randint(1,1000))
 test = data.drop(train.index)
 
-A, B, C = [1, 2], [2, 3], [1, 3]
+A, B, C = [1, 2], [1, 3], [2, 3]
 train.rename(columns={train.columns[-1]:'label'}, inplace=True)
-train_1 = train.loc[train['label'].isin(A)]
-train_2 = train.loc[train['label'].isin(B)]
-train_3 = train.loc[train['label'].isin(C)]
+train_12 = train.loc[train['label'].isin(A)]
+train_13 = train.loc[train['label'].isin(B)]
+train_23 = train.loc[train['label'].isin(C)]
 
-theta_1 = LogisticRegression(train, theta, iters, alpha, 1)
-theta_2 = LogisticRegression(train, theta, iters, alpha, 2)
-theta_3 = LogisticRegression(train, theta, iters, alpha, 3)
+theta_12 = LogisticRegression(train_12, theta, iters, alpha, 1, 2)
+theta_13 = LogisticRegression(train_13, theta, iters, alpha, 1, 3)
+theta_23 = LogisticRegression(train_23, theta, iters, alpha, 2, 3)
 
-a1, a2, a3, a = Accuracy(test, theta_1, theta_2, theta_3)
+a1, a2, a3, a = Accuracy(test, theta_12, theta_13, theta_23)
 print("One vs One individual class accuracies: {}, {}, {}".format(a1, a2, a3))
 print("One vs One overall accuracy: {}".format(a))
